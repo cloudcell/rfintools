38769a034be76a00d0e62ba29edebb4ef52a9e31
cloudcello <ef256aff@opayq.com>
2015-12-21 19:39:46 +0300

    makes a tiny fix related to displaying a "adjusted duplicate" file name properly

--- R/robustApplyParamset.R
+++ R/robustApplyParamset.R
@@ -19,20 +19,19 @@ backupResult <- function(baseDir=NULL, jobSubDir=NULL, objectName=NULL)
     ### ATTN! row.names(param.combo) can be used only within quantstrat apply.paramset
     ### FIXME! get this combo name from the 'result' if possible
     comboName <- row.names(param.combo)
-    cat(paste0("combo name = ", comboName, "\n"))
+    cat(paste0("Backup data file [combo.number].RData = ", comboName, ".Rdata\n"))
     baseFileName = paste0(comboName,".RData")
-    fullFileName = paste0(backupPath,"/",baseFileName)
+    fullFileName = paste0(backupPath, "/", baseFileName)
 
     if(1) { # disregard for now
         # name this worker output as a duplicate
         if(file.exists(fullFileName)) {
             cat(paste0("baseFileName = ", baseFileName, " already exists.\n"))
             fullFileName <- tempfile(pattern=paste0(comboName,"_DUP_"), tmpdir=backupPath, fileext=".RData")
-            baseFileName <- substr(fullFileName, start = length(backupPath), stop = length(fullFileName))
-            cat(paste0("Saving data as a duplicate ", baseFileName, "\n"))
+            baseFileName <- substr(fullFileName, start = length(backupPath)+1L, stop = length(fullFileName))
+            cat(paste0("Saving data as a duplicate [ ", baseFileName, " ]\n"))
         }
     }
-
 
     # save(list=objectName, file="//server/data_01/aa_cluster_backups/dummy_var.RData")
     save(list=objectName, file=fullFileName )
@@ -40,6 +39,32 @@ backupResult <- function(baseDir=NULL, jobSubDir=NULL, objectName=NULL)
 
 }
 
+    # substr("abcd",2,3)
+if(0){
+    baseDir="//octopus/data_01/aa_cluster_backup"
+    jobSubDir="testFailSafe"
+    backupPath <- paste0(baseDir,"/", jobSubDir)
+    comboName <- 1
+    cat(paste0("Backup data file [combo.number].RData = ", comboName, ".Rdata\n"))
+    baseFileName = paste0(comboName,".RData")
+    fullFileName = paste0(backupPath, "/", baseFileName)
+
+    if(1) { # disregard for now
+        # name this worker output as a duplicate
+        if(file.exists(fullFileName)) {
+            cat(paste0("baseFileName = ", baseFileName, " already exists.\n"))
+            fullFileName <- tempfile(pattern=paste0(comboName,"_DUP_"), tmpdir=backupPath, fileext=".RData")
+
+            baseFileName <- substr(fullFileName, start = nchar(backupPath)+1L, stop = nchar(fullFileName))
+
+            # Workers running on Windows will have tempfile() produce a path with a backslash (as "\\")
+            # so 1 extra character must be dealt with separately:
+            if(substr(baseFileName,1,1)=="\\") baseFileName <- substr(baseFileName,2,nchar(baseFileName))
+
+            cat(paste0("Saving data as a duplicate [ ", baseFileName, " ]\n"))
+        }
+    }
+}
 
 
 
