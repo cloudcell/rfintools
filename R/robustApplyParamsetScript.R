7e237d7d5cedd8308a6df9df96d9cb2229763a1d
cloudcello <ef256aff@opayq.com>
2015-12-26 08:26:41 +0300

    makes indentation to help find installation bug

--- R/robustApplyParamsetScript.R
+++ R/robustApplyParamsetScript.R
@@ -93,176 +93,178 @@ stats <- results$tradeStats
 #-------------------------------------------------------------------------------
 
 if(0) {
-setwd('y:/_git_repository_r/testr/R_research/sma1/')
-source("sma1.M3.paramset.sma.R")
-
-if(0) {
-
-# require(rfintools)
-require(quantstrat)
-require(doRedis)
-require(rfintools)
-
-
-if(0) {
-
-
-    # commandArgs
-    # trailingOnly	-- logical. Should only arguments after --args be returned?
-    # If trailingOnly = TRUE, a character vector of those arguments (if any) supplied after --args.
-
-    cmdLineArgs <- commandArgs(trailingOnly=TRUE)
-    print(paste("cmdLineArgs:",cmdLineArgs))
-
-    settingsVarName <- load(cmdLineArgs[1])
-    print(paste("settingsVarName:", settingsVarName, " --> printing structure:"))
-    settingsVar <- get(settingsVarName)
-    print(str(settingsVar))
-
-    # objectsVector.char <- load(file="robustApplyParamsetParams.RData")
-    # print(paste("objectsVector.char:", objectsVector.char))
-
-    ar <- settingsVar # applyStrategyArgs
-    str(ar)
-
-    rc <- try(get("ar$paramsets"), silent = TRUE)
-    if(!(class(rc)=="try-error")) {
-        paramsets=ar$paramsets
-    }
-
-    # print(paste("missing:", missing(a$paramsets))
-    # paramsets=a$paramsets
-    # if(is.null(paramsets)){
-    #     rm(paramsets)
-    # }
-
-    args <- substitute(list(...))[-1]
-    print(args)
-
-    argnames <- names(args)
-    print(argnames)
-
-} else {
-
-}
-
-# assign()
-
-# make a loop to assign(arg, value & perhaps pass those obj's to app.paramset)
-
-# save an .RData file with (host name and) process id, so the
-# process can be monitored
-
-if(0) {
-
-    if(0)    print(settingsVarName)
-
-    if(0)    importedStrategy.st= strategyToExport
-    if(0)    strategy.st    = ar$strategy.st
-    if(1)    paramset.label = ar$paramset.label
-    if(0)    portfolio.st   = ar$portfolio.st
-    if(0)    account.st     = ar$account.st
-    if(0)    mktdata        = ar$mktdata
-    if(0)    nsamples       = ar$nsamples
-    if(1)    user.func      = ar$user.func
-    if(1)    user.args      = ar$user.args
-    if(1)    calc           = ar$calc
-    if(0)    audit          = ar$audit
-    if(1)    packages       = ar$packages
-    if(0)    verbose        = ar$verbose
-    if(0)    verbose.wrk    = ar$verbose.wrk
-    if(1)    doRedisHost    = ar$doRedisHost
-
-    if(0) { # now that all this is generated inside the script
-        # ".strategy"
-        .strategy <- new.env()
-
-        assign("strategyName",strategyName)
-        assign(strategyName,importedStrategy.st)
-
-        assign(x=strategyName,value=importedStrategy.st, pos=.strategy)
-
-        ls(pos=.strategy) #, envir=".strategy")
-        # environmentName(e, "test")
-        # str(e)
-        # class(e)
-        # environmentName(e) <- "test"
-
-    }
-
-
-
-}
-
-
-
-if(1) {
-
-#-------------------------------------------------------------------------------
     setwd('y:/_git_repository_r/testr/R_research/sma1/')
-
-    source("sma1.AA.include.R")
-    source("sma1.AD.getSymbols.R")
-    # source("sma1.M1.strategy.basic.R")()
-    source("sma1.M2.add.paramsets.R")
-#-------------------------------------------------------------------------------
-
-    require(doRedis)
-    options('redis:num'=TRUE) # prevents the nasty bug
-    registerDoRedis('jobs', host="192.168.xxx.xxx")#doRedisHost)
+    source("sma1.M3.paramset.sma.R")
 
     if(0) {
-        # a special case is needed for this !
-        registerDoSEQ() #('jobs', host="192.168.xxx.xxx")
+
+        # require(rfintools)
+        require(quantstrat)
+        require(doRedis)
+        require(rfintools)
+
+
+        if(0) {
+
+
+            # commandArgs
+            # trailingOnly	-- logical. Should only arguments after --args be returned?
+            # If trailingOnly = TRUE, a character vector of those arguments (if any) supplied after --args.
+
+            cmdLineArgs <- commandArgs(trailingOnly=TRUE)
+            print(paste("cmdLineArgs:",cmdLineArgs))
+
+            settingsVarName <- load(cmdLineArgs[1])
+            print(paste("settingsVarName:", settingsVarName, " --> printing structure:"))
+            settingsVar <- get(settingsVarName)
+            print(str(settingsVar))
+
+            # objectsVector.char <- load(file="robustApplyParamsetParams.RData")
+            # print(paste("objectsVector.char:", objectsVector.char))
+
+            ar <- settingsVar # applyStrategyArgs
+            str(ar)
+
+            rc <- try(get("ar$paramsets"), silent = TRUE)
+            if(!(class(rc)=="try-error")) {
+                paramsets=ar$paramsets
+            }
+
+            # print(paste("missing:", missing(a$paramsets))
+            # paramsets=a$paramsets
+            # if(is.null(paramsets)){
+            #     rm(paramsets)
+            # }
+
+            args <- substitute(list(...))[-1]
+            print(args)
+
+            argnames <- names(args)
+            print(argnames)
+
+        } else {
+
+        }
+
+        # assign()
+
+        # make a loop to assign(arg, value & perhaps pass those obj's to app.paramset)
+
+        # save an .RData file with (host name and) process id, so the
+        # process can be monitored
+
+        if(0) {
+
+            if(0)    print(settingsVarName)
+
+            if(0)    importedStrategy.st= strategyToExport
+            if(0)    strategy.st    = ar$strategy.st
+            if(1)    paramset.label = ar$paramset.label
+            if(0)    portfolio.st   = ar$portfolio.st
+            if(0)    account.st     = ar$account.st
+            if(0)    mktdata        = ar$mktdata
+            if(0)    nsamples       = ar$nsamples
+            if(1)    user.func      = ar$user.func
+            if(1)    user.args      = ar$user.args
+            if(1)    calc           = ar$calc
+            if(0)    audit          = ar$audit
+            if(1)    packages       = ar$packages
+            if(0)    verbose        = ar$verbose
+            if(0)    verbose.wrk    = ar$verbose.wrk
+            if(1)    doRedisHost    = ar$doRedisHost
+
+            if(0) { # now that all this is generated inside the script
+                # ".strategy"
+                .strategy <- new.env()
+
+                assign("strategyName",strategyName)
+                assign(strategyName,importedStrategy.st)
+
+                assign(x=strategyName,value=importedStrategy.st, pos=.strategy)
+
+                ls(pos=.strategy) #, envir=".strategy")
+                # environmentName(e, "test")
+                # str(e)
+                # class(e)
+                # environmentName(e) <- "test"
+
+            }
+
+
+
+        }
+
+
+
+        if(1) {
+
+            #-------------------------------------------------------------------------------
+            setwd('y:/_git_repository_r/testr/R_research/sma1/')
+
+            source("sma1.AA.include.R")
+            source("sma1.AD.getSymbols.R")
+            # source("sma1.M1.strategy.basic.R")()
+            source("sma1.M2.add.paramsets.R")
+            #-------------------------------------------------------------------------------
+
+            require(doRedis)
+            options('redis:num'=TRUE) # prevents the nasty bug
+            registerDoRedis('jobs', host="192.168.xxx.xxx")#doRedisHost)
+
+            if(0) {
+                # a special case is needed for this !
+                registerDoSEQ() #('jobs', host="192.168.xxx.xxx")
+            }
+
+
+            symbols=c("MSFT")
+            currency='USD'
+
+            print(paste("running apply.paramset()..."))
+
+            ### blotter
+
+            # initPortf(portfolio.st, symbols='MSFT', initDate=initDate, currency='CCY')
+            # initPortf(portfolio.st=ar$portfolio.st, symbols=ar$symbols, initDate=initDate, currency=currency)
+            initPortf(portfolio.st, symbols, initDate=initDate, currency)
+            # initAcct(account.st, portfolios=portfolio.st, initDate=initDate, currency=currency)
+            initAcct(account.st, portfolios=portfolio.st, initDate, currency)
+
+            ### quantstrat
+
+            initOrders(portfolio.st, initDate=initDate)
+
+            cat("about to enter apply.paramset()\n")
+            # run the apply.paramset() the usual way
+            results <- apply.paramset(strategy.st    = strategy.st,#ar$strategy.st,
+                                      paramset.label = "SMA",#ar$paramset.label,
+                                      portfolio.st   = portfolio.st,
+                                      account.st     = account.st,
+                                      # mktdata        = ar$mktdata,
+                                      nsamples       = .nsamples,#ar$nsamples,
+                                      # user.func      = ar$user.func,
+                                      # user.args      = ar$user.args,
+                                      # calc           = ar$calc,
+                                      # audit          = ar$audit,
+                                      # packages       = ar$packages,
+                                      verbose        = TRUE#ar$verbose,
+                                      # verbose.wrk    = ar$verbose.wrk
+            )
+
+        }
+        # save output with the result into an .RData file
+
+        cat("flushing redis\n")
+
+        removeQueue('jobs')
+        redisFlushDB()
+
+        cat("finished!\n")
+
+
     }
 
-
-    symbols=c("MSFT")
-    currency='USD'
-
-    print(paste("running apply.paramset()..."))
-
-    ### blotter
-
-    # initPortf(portfolio.st, symbols='MSFT', initDate=initDate, currency='CCY')
-    # initPortf(portfolio.st=ar$portfolio.st, symbols=ar$symbols, initDate=initDate, currency=currency)
-    initPortf(portfolio.st, symbols, initDate=initDate, currency)
-    # initAcct(account.st, portfolios=portfolio.st, initDate=initDate, currency=currency)
-    initAcct(account.st, portfolios=portfolio.st, initDate, currency)
-
-    ### quantstrat
-
-    initOrders(portfolio.st, initDate=initDate)
-
-    cat("about to enter apply.paramset()\n")
-    # run the apply.paramset() the usual way
-    results <- apply.paramset(strategy.st    = strategy.st,#ar$strategy.st,
-                              paramset.label = "SMA",#ar$paramset.label,
-                              portfolio.st   = portfolio.st,
-                              account.st     = account.st,
-                              # mktdata        = ar$mktdata,
-                              nsamples       = .nsamples,#ar$nsamples,
-                              # user.func      = ar$user.func,
-                              # user.args      = ar$user.args,
-                              # calc           = ar$calc,
-                              # audit          = ar$audit,
-                              # packages       = ar$packages,
-                              verbose        = TRUE#ar$verbose,
-                              # verbose.wrk    = ar$verbose.wrk
-                              )
-
-}
-# save output with the result into an .RData file
-
-cat("flushing redis\n")
-
-removeQueue('jobs')
-redisFlushDB()
-
-cat("finished!\n")
-
-
 }
 
-}
+
 
