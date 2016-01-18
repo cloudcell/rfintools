e22600434537bfa83a339bff0581c8dd678dfa62
cloudcello <kodak55555@gmail.com>
2016-01-18 16:31:38 +0300

    commits an actual test

--- tests/test_tradeStatsExt.R
+++ tests/test_tradeStatsExt.R
@@ -1,10 +1,20 @@
+# reference on testing packages:
+# http://bioconductor.org/developers/unitTesting-guidelines/
 
+# To start using testing packages quickly, here is
+# the bare minimum: http://www.johnmyleswhite.com/notebook/2010/08/17/unit-testing-in-r-the-bare-minimum/
+
+# The following test function was borrowed from the package 'rredis'
+# checkEquals <- function(x, y) if(!isTRUE(all.equal(x, y, check.attributes=FALSE))) stop()
 
 # require(RUnit)
 require(blotter)
 require(quantstrat)
 require(rfintools)
 
+options(digits=22) # default == 7
+
+checkEquals <- function(x, y) if(!isTRUE(all.equal(x, y, check.attributes=FALSE))) stop()
 
 # dir=system.file('data',package='quantstrat')
 dir=system.file('extdata',package='rfintools')
@@ -12,130 +22,107 @@ load(file = paste0(dir,"/","test_tradeStats.RData"))
 
 put.portfolio(portf2,portfolio.st = "forex")
 
-getPortfolio("forex")$symbols$GBPUSD$txn
-getPortfolio("forex")$symbols$GBPUSD$posPL
+if(0) {
+    getPortfolio("forex")$symbols$GBPUSD$txn
+    getPortfolio("forex")$symbols$GBPUSD$posPL
+}
 
-# full data --------------------------------------------------------------------
+# full data ------------------------------------------------------------------ -
 ts <- tradeStatsExt("forex","GBPUSD", debugF = TRUE)
 
 str(ts)
+# checkEquals( ts'ata.frame':	1 obs. of  40 variables:
+checkEquals( levels(ts$Portfolio)         , "forex" )
+checkEquals( levels(ts$Symbol)            , "GBPUSD" )
+checkEquals( ts$Num.Txns                  , 24 )
+checkEquals( ts$Num.Trades                , 12 )
+checkEquals( ts$Net.Trading.PL            , -492 )
+checkEquals( ts$Avg.Trade.PL              , -41 )
+checkEquals( ts$Med.Trade.PL              , -136 )
+checkEquals( ts$Largest.Winner            , 414 )
+checkEquals( ts$Largest.Loser             , -396 )
+checkEquals( ts$Gross.Profits             , 1296 )
+checkEquals( ts$Gross.Losses              , -1788 )
+checkEquals( ts$Std.Dev.Trade.PL          , 285.3865 )
+checkEquals( ts$Percent.Positive          , 33.333333333333329 )
+checkEquals( ts$Percent.Negative          , 66.666666666666657)
+checkEquals( ts$Profit.Factor             , 0.72483221476512438 )
+checkEquals( ts$Avg.Win.Trade             , 324 )
+checkEquals( ts$Med.Win.Trade             , 359 )
+checkEquals( ts$Avg.Losing.Trade          , -223.5 )
+checkEquals( ts$Med.Losing.Trade          , -241 )
+checkEquals( ts$Avg.Daily.PL              , -61.5 )
+checkEquals( ts$Med.Daily.PL              , -121 )
+checkEquals( ts$Std.Dev.Daily.PL          , 377.67975852566377 )
+checkEquals( ts$Ann.Sharpe                , -2.5849471986368129 )
+checkEquals( ts$Max.Drawdown              , -1424 )
+checkEquals( ts$Profit.To.Max.Draw        , -0.3455056179775281 )
+checkEquals( ts$Avg.WinLoss.Ratio         , 1.4496644295302488 )
+checkEquals( ts$Med.WinLoss.Ratio         , 1.4896265560166377 )
+checkEquals( ts$Max.Equity                , 400 )
+checkEquals( ts$Min.Equity                , -1024 )
+checkEquals( ts$End.Equity                , -492 )
+checkEquals( ts$Max.Consec.Winning.Trades , 2 )
+checkEquals( ts$Max.Consec.Losing.Trades  , 4 )
+checkEquals( ts$Avg.Bars.In.Total.Trades  , 22.58333333 )
+checkEquals( ts$Avg.Bars.In.Winning.Trades, 27 )
+checkEquals( ts$Avg.Bars.In.Losing.Trades , 20.375 )
+checkEquals( ts$Max.Bars.Flat.Period      , 51 )
+checkEquals( ts$Percent.Time.In.Market    , 68.090452261306538 )
+checkEquals( ts$RINA.Index                , -0.90635296297990031 )
+checkEquals( ts$Date.Min                  , "2002-10-21 04:30:00" )
+checkEquals( ts$Date.Max                  , "2002-11-05 02:00:00" )
+
+# scoped data ---------------------------------------------------------------- -
+
+ts <- tradeStatsExt("forex","GBPUSD", Dates = "2002-10-22::2002-10-30")
+
+str(ts)
+
 # 'data.frame':	1 obs. of  40 variables:
-#     $ Portfolio                 : Factor w/ 1 level "forex": 1
-# $ Symbol                    : Factor w/ 1 level "GBPUSD": 1
-# $ Num.Txns                  : num 24
-# $ Num.Trades                : int 12
-# $ Net.Trading.PL            : num -492
-# $ Avg.Trade.PL              : num -41
-# $ Med.Trade.PL              : num -136
-# $ Largest.Winner            : num 414
-# $ Largest.Loser             : num -396
-# $ Gross.Profits             : num 1296
-# $ Gross.Losses              : num -1788
-# $ Std.Dev.Trade.PL          : num 285
-# $ Percent.Positive          : num 33.3
-# $ Percent.Negative          : num 66.7
-# $ Profit.Factor             : num 0.725
-# $ Avg.Win.Trade             : num 324
-# $ Med.Win.Trade             : num 359
-# $ Avg.Losing.Trade          : num -223
-# $ Med.Losing.Trade          : num -241
-# $ Avg.Daily.PL              : num -61.5
-# $ Med.Daily.PL              : num -121
-# $ Std.Dev.Daily.PL          : num 378
-# $ Ann.Sharpe                : num -2.58
-# $ Max.Drawdown              : num -1424
-# $ Profit.To.Max.Draw        : num -0.346
-# $ Avg.WinLoss.Ratio         : num 1.45
-# $ Med.WinLoss.Ratio         : num 1.49
-# $ Max.Equity                : num 400
-# $ Min.Equity                : num -1024
-# $ End.Equity                : num -492
-# $ Max.Consec.Winning.Trades : int 2
-# $ Max.Consec.Losing.Trades  : int 4
-# $ Avg.Bars.In.Total.Trades  : num 22.6
-# $ Avg.Bars.In.Winning.Trades: num 27
-# $ Avg.Bars.In.Losing.Trades : num 20.4
-# $ Max.Bars.Flat.Period      : int 51
-# $ Percent.Time.In.Market    : num 68.1
-# $ RINA.Index                : num -0.906
-# $ Date.Min                  : POSIXct, format: "2002-10-21 04:30:00"
-# $ Date.Max                  : POSIXct, format: "2002-11-05 02:00:00"
-
-# full data --------------------------------------------------------------------
+checkEquals( levels(ts$Portfolio)          , "forex" )
+checkEquals( levels(ts$Symbol)             , "GBPUSD" )
+checkEquals( ts$Num.Txns                   , 15 )
+checkEquals( ts$Num.Trades                 , 7 )
+checkEquals( ts$Net.Trading.PL             , -532 )
+checkEquals( ts$Avg.Trade.PL               , -87.428571428565633 )
+checkEquals( ts$Med.Trade.PL               , -126 )
+checkEquals( ts$Largest.Winner             , 364 )
+checkEquals( ts$Largest.Loser              , -396 )
+checkEquals( ts$Gross.Profits              , 528 )
+checkEquals( ts$Gross.Losses               , -1140 )
+checkEquals( ts$Std.Dev.Trade.PL           , 263.97149918306059 )
+checkEquals( ts$Percent.Positive           , 28.571428571428569 )
+checkEquals( ts$Percent.Negative           , 71.428571428571431 )
+checkEquals( ts$Profit.Factor              , 0.46315789473686186 )
+checkEquals( ts$Avg.Win.Trade              , 264 )
+checkEquals( ts$Med.Win.Trade              , 264 )
+checkEquals( ts$Avg.Losing.Trade           , -228 )
+checkEquals( ts$Med.Losing.Trade           , -246 )
+checkEquals( ts$Avg.Daily.PL               , -122.39999999999191 )
+checkEquals( ts$Med.Daily.PL               , -126 )
+checkEquals( ts$Std.Dev.Daily.PL           , 421.5765648135594 )
+checkEquals( ts$Ann.Sharpe                 , -4.6089842866502995 )
+checkEquals( ts$Max.Drawdown               , -1424 )
+checkEquals( ts$Profit.To.Max.Draw         , -0.37359550561797755 )
+checkEquals( ts$Avg.WinLoss.Ratio          , 1.1578947368421546 )
+checkEquals( ts$Med.WinLoss.Ratio          , 1.0731707317073498 )
+checkEquals( ts$Max.Equity                 , 400 )
+checkEquals( ts$Min.Equity                 , -1024 )
+checkEquals( ts$End.Equity                 , -532 )
+checkEquals( ts$Max.Consec.Winning.Trades  , 2 )
+checkEquals( ts$Max.Consec.Losing.Trades   , 4 )
+checkEquals( ts$Avg.Bars.In.Total.Trades   , 24.142857142857142 )
+checkEquals( ts$Avg.Bars.In.Winning.Trades , 24 )
+checkEquals( ts$Avg.Bars.In.Losing.Trades  , 24.2 )
+checkEquals( ts$Max.Bars.Flat.Period       , 19 )
+checkEquals( ts$Percent.Time.In.Market     , 80.952380952380949 )
+checkEquals( ts$RINA.Index                 , -0.91709991492573573 )
+checkEquals( ts$Date.Min                   , "2002-10-22" )
+checkEquals( ts$Date.Max                   , "2002-10-30 02:00:00" )
 
 
+# t(tradeStatsExt("forex","GBPUSD", Dates = "2002-10-23::2002-10-30", debugF = TRUE))
+# t(tradeStatsExt("forex","GBPUSD", Dates = "2002-10-21 00:00 UST::2002-10-30 00:00 UST",debugF = TRUE))
 
-
-
-# getPortfolio("forex")$symbols$GBPUSD$txn["2002-10-21::2002-10-31"]
-
-ts2 <- tradeStatsExt("forex","GBPUSD", Dates = "2002-10-22::2002-10-31")
-
-str(ts2)
-
-str(ts2)
-# 'data.frame':	1 obs. of  40 variables:
-#     $ Portfolio                 : Factor w/ 1 level "forex": 1
-# $ Symbol                    : Factor w/ 1 level "GBPUSD": 1
-# $ Num.Txns                  : num 16
-# $ Num.Trades                : int 8
-# $ Net.Trading.PL            : num -478
-# $ Avg.Trade.PL              : num -110
-# $ Med.Trade.PL              : num -186
-# $ Largest.Winner            : num 364
-# $ Largest.Loser             : num -396
-# $ Gross.Profits             : num 528
-# $ Gross.Losses              : num -1406
-# $ Std.Dev.Trade.PL          : num 252
-# $ Percent.Positive          : num 25
-# $ Percent.Negative          : num 75
-# $ Profit.Factor             : num 0.376
-# $ Avg.Win.Trade             : num 264
-# $ Med.Win.Trade             : num 264
-# $ Avg.Losing.Trade          : num -234
-# $ Med.Losing.Trade          : num -251
-# $ Avg.Daily.PL              : num -146
-# $ Med.Daily.PL              : num -191
-# $ Std.Dev.Daily.PL          : num 382
-# $ Ann.Sharpe                : num -6.09
-# $ Max.Drawdown              : num -1424
-# $ Profit.To.Max.Draw        : num -0.336
-# $ Avg.WinLoss.Ratio         : num 1.13
-# $ Med.WinLoss.Ratio         : num 1.05
-# $ Max.Equity                : num 400
-# $ Min.Equity                : num -1024
-# $ End.Equity                : num -478
-# $ Max.Consec.Winning.Trades : int 2
-# $ Max.Consec.Losing.Trades  : int 4
-# $ Avg.Bars.In.Total.Trades  : num 23.9
-# $ Avg.Bars.In.Winning.Trades: num 24
-# $ Avg.Bars.In.Losing.Trades : num 23.8
-# $ Max.Bars.Flat.Period      : int 19
-# $ Percent.Time.In.Market    : num 78.8
-# $ RINA.Index                : num -1.29
-# $ Date.Min                  : POSIXct, format: "2002-10-22"
-# $ Date.Max                  : POSIXct, format: "2002-10-31 23:30:00"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-t(tradeStatsExt("forex","GBPUSD", Dates = "2002-10-23::2002-10-30", debugF = TRUE))
-t(tradeStatsExt("forex","GBPUSD", Dates = "2002-10-21 00:00 UST::2002-10-30 00:00 UST",debugF = TRUE))
-
-
-out=0
-
-out
-
+# done!
